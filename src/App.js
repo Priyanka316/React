647016248dd2d7c7b9a0950531162365f2a50e87
Priyanka316 <107699187+Priyanka316@users.noreply.github.com>
2022-10-03 00:00:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Router, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './assign4/Home';
 import ContactUs from './assign4/ContactUs';
 import Students from './assign4/Students';
@@ -10,11 +10,11 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Router>
+        <Routes>
           <Route path='/' element = {<Home/>} ></Route>
           <Route path='/ContactUs' element = {<ContactUs/>} ></Route>
           <Route path='/Students' element = {<Students/>} ></Route>
-        </Router>
+        </Routes>
       </BrowserRouter>
     </div>
   );
