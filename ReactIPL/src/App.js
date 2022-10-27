61acbcc33c8956ef2deaa40e225f62f20980dd53
Priyanka316 <107699187+Priyanka316@users.noreply.github.com>
2022-10-27 20:00:08 +0530

    Add files via upload

---
+++ ReactIPL/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Parent from './UseState/Parent';
+import Usereducer from './Usereducer';
+import UseMemo from './UseMemo'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>    
+      <Parent/>
+      <Usereducer/>
+      <UseMemo/>
+    </div>
+  );
+}
+
+export default App;
