b79d07004d51b39e424bfe3ec246957fcc46e298
Priyanka316 <107699187+Priyanka316@users.noreply.github.com>
2022-10-27 15:16:46 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Components/Home';
-import Students from './Components/Students';
-import ContactUs from './Components/Contactus';
-import AddStd from './Components/AddStd';
-import EditStd from './Components/EditStd';
-import Link from './Components/Link';
+import Home from './assign7/Home';
+import Students from './assign7/Students';
+import ContactUs from './assign7/Contactus';
+import AddStd from './assign7/AddStd';
+import EditStd from './assign7/EditStd';
+import Link from './assign7/Link';
 import { useState } from 'react';
 
 
---
+++ src/assign7/Home.jsx
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const Home = () =>{
+    return(
+        <div>        
+            <p id="TitlePg">Home</p>
+        </div>
+    )
+}
+
+export default Home;
